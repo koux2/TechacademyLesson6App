d3ab04cdb7a6af5522c4f00e2f85850a879a241c
koux2 <koux2.g@gmail.com>
2021-03-22 19:53:04 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by koux2 on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
